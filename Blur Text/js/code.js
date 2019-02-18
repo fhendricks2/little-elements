a60ffb3633d44c41802726e57e25424f1dfc124f
fhendricks2 <f.rubymelody@gmail.com>
2019-02-18 11:52:54 +0200

    blur text

---
+++ Blur Text/js/code.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+    var nodes = document.querySelectorAll(".blur>*");
+    for (var i=0; i<nodes.length; i++){
+        nodes[i].style.animationDelay = (i*3) + "s";
+    }
+})
+
+console.log(nodes)
